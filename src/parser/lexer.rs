5f509e8ded35c229d4b75cb373d84135b01047eb
flappibird123 <jasonlilb2@gmail.com>
2025-11-12 22:22:34 +0000

    Added basic enum types for lexer

---
+++ src/parser/lexer.rs
@@ -0,0 +1,45 @@
+struct Token {
+    kind: TokenKind,
+    value: Option<String>,
+    line: usize,
+    column: usize,
+}
+
+enum TokenKind {
+    KeyWord(String),
+    Identifier(String),
+    Type(Type),
+    Operator(Operator),
+    Literal(String),
+    Symbol(Symbol),
+}
+
+enum Type {
+    Int,
+    Float,
+    Char,
+    Boolean,
+}
+
+enum Operator {
+    Plus,
+    Minus,
+    Multiply,
+    Division,
+    Assignment,
+}
+
+enum KeyWord {
+    Let,
+    Const,
+    Print,
+}
+
+enum Symbol {
+    LParen,
+    RParen,
+    LDblQuote,
+    RDblQuote,
+    SemiColon,
+}
+
