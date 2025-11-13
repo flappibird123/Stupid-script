d8e2b9e053ee7aa6344c244501ac0805ab288444
flappibird123 <jasonlilb2@gmail.com>
2025-11-13 23:16:33 +0000

    fixed file name Cargo.toml

---
+++ target/debug/deps/stupid_script-ff0ba0332314d925.d
@@ -0,0 +1,7 @@
+/workspaces/Stupid-script/target/debug/deps/stupid_script-ff0ba0332314d925.d: src/lib.rs
+
+/workspaces/Stupid-script/target/debug/deps/libstupid_script-ff0ba0332314d925.rlib: src/lib.rs
+
+/workspaces/Stupid-script/target/debug/deps/libstupid_script-ff0ba0332314d925.rmeta: src/lib.rs
+
+src/lib.rs:
