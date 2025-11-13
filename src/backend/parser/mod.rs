963dbee9cd96192e7fe125140bcdc14201f79684
flappibird123 <jasonlilb2@gmail.com>
2025-11-13 23:17:54 +0000

    fixed issue in lib.rs

--- src/backend/parser/mod.rs
+++ src/backend/parser/mod.rs
@@ -1,2 +1,3 @@
-mod parser;
+pub mod parser;
+
 pub use parser::Parser;
---
+++ target/debug/deps/stupid_script-0b1c3a3991fa5a5c.d
@@ -0,0 +1,9 @@
+/workspaces/Stupid-script/target/debug/deps/stupid_script-0b1c3a3991fa5a5c.d: src/main.rs src/runtime/mod.rs src/runtime/value.rs src/runtime/env.rs src/runtime/interpreter.rs
+
+/workspaces/Stupid-script/target/debug/deps/stupid_script-0b1c3a3991fa5a5c: src/main.rs src/runtime/mod.rs src/runtime/value.rs src/runtime/env.rs src/runtime/interpreter.rs
+
+src/main.rs:
+src/runtime/mod.rs:
+src/runtime/value.rs:
+src/runtime/env.rs:
+src/runtime/interpreter.rs:
